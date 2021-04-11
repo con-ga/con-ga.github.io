7a862573c3970418fa08407ab52903d855127b30
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-11 10:14:17 +0700

    Add files via upload

---
+++ try/sw-cors/sw-cors.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  'index.htm',
+  'main.css',
+  'main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+hat you rewrite your code to use async/await, as it's much more readable (and supported by any browser that also supports service workers today).
+
+addEventListener("fetch", function(e) {
+  e.respondWith((async function() {
+    const cachedResponse = await caches.match(e.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    const networkResponse = await fetch(e.request);
+
+    const hosts = [
+      'https://www.google.com'
+    ];
+
+    if (hosts.some((host) => e.request.url.startsWith(host))) {
+      // This clone() happens before `return networkResponse` 
+      const clonedResponse = networkResponse.clone();
+
+      e.waitUntil((async function() {
+        const cache = await caches.open(CACHE_NAME);
+        // This will be called after `return networkResponse`
+        // so make sure you already have the clone!
+        await cache.put(e.request, clonedResponse);
+      })());
+    }
+
+    return networkResponse;
+  })());
+});
