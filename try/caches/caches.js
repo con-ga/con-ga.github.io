42262e15258517066e57880e5629e27528b72e6f
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-15 19:53:05 +0700

    Update caches.js

--- try/caches/caches.js
+++ try/caches/caches.js
@@ -41,15 +41,14 @@ self.addEventListener('message', function(ev) {
             caches.open(CACHE_NAME).then(function(cache) {
                 return cache.keys();
             }).then(function (keys) {
-                keys.forEach(key => console.log(key.url));
-            });/*
                 return keys.map(request => request.url);
             }).then(function (urls) {
                 console.log("xyztu: " + ev.data);
                 urls.forEach(console.log);
                 clients.forEach(function(client, ind, arr) {
                     client.postMessage( {msg: ev.data, urls: urls});
-            });*/
+                });
+            });
         }
     });
 });
