608cc87b8b96155feeb74c189294fe8d1f135dce
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-15 07:30:32 +0700

    Update caches.js

--- try/caches/caches.js
+++ try/caches/caches.js
@@ -4,7 +4,7 @@ var urlsToCache = [
   '/try/caches/mot.htm',
   '/try/caches/hai.htm',
   '/try/caches/ba.htm',
-  '/try/caches/lst8.htm'
+  '/try/caches/lst9.htm'
 ];
 console.log("service worker global");
 self.addEventListener('install', function(event) {
