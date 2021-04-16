ca09f3cf58f42b27a8a2d57a11f9fd0c2b7ba7e8
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-16 22:10:52 +0700

    Update page-dynamic.js

--- try/dyn-pages/page-dynamic.js
+++ try/dyn-pages/page-dynamic.js
@@ -2,6 +2,6 @@ self.addEventListener("install", function(ev) {
 });
 self.addEventListener("fetch", function(ev) {
     ev.respondWith(
-        return Promise.resolve(new Response("(" + ev.request.url + ")<hr />");
+        Promise.resolve(new Response("(" + ev.request.url + ")<hr />");
     );
 });
