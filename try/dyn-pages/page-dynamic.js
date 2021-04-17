737bfc3b63472ec88cc6168471b469a9f3f0c758
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-17 09:01:57 +0700

    Update page-dynamic.js

--- try/dyn-pages/page-dynamic.js
+++ try/dyn-pages/page-dynamic.js
@@ -1,6 +1,13 @@
 self.addEventListener("install", function(ev) {
 });
 self.addEventListener("fetch", function(ev) {
+    const ind = ev.request.url.indexOf("google");
+    if (ind >= 0) {
+        fetch(ev.request.url).then(function(response) {
+            ev.respondWith(Promise.resolve(response));
+            return;
+        });
+    }
     var myHeaders = new Headers();
     myHeaders.append('Content-Type', 'text/html');
 
