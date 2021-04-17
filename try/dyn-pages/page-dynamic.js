3a15ec90585f5edd0ff1f8fc85c3388f79a6655a
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-17 11:36:16 +0700

    Update page-dynamic.js

--- try/dyn-pages/page-dynamic.js
+++ try/dyn-pages/page-dynamic.js
@@ -5,7 +5,7 @@ self.addEventListener("fetch", function(ev) {
     const ind = ev.request.url.indexOf("google");
     if (ind >= 0) {
         fetch(ev.request.url, {mode : 'no-cors'}).then(function(response) {
-            ev.respondWith(Promise.resolve(response));
+            ev.respondWith(Promise.resolve(response.clone()));
             
         });
     } else {
