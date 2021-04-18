1256554db143aa2db6658c345254ab469963a6f5
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-18 15:21:49 +0700

    Update cors-url.js

--- try/cors-url/cors-url.js
+++ try/cors-url/cors-url.js
@@ -5,7 +5,7 @@ self.addEventListener("fetch", function(ev) {
         const i = ev.request.url.indexOf("?iframe=");
         let headers = new Headers({"Content-Type" : "text/html"});
         ifr = ev.request.url.substring(i + 8);
-        let html = "<body><iframe src='" + ifr + "' width=500 height=400></iframe><hr />" + "<iframe width=500 height=400 src='page.x?url=" + ifr + "'></iframe></body>";
+        let html = "<body>" + ev.request.url + "<hr /><iframe src='" + ifr + "' width=500 height=400></iframe><hr />" + "<iframe width=500 height=400 src='page.x?url=" + ifr + "'></iframe></body>";
         ev.respondWith(new Response(html, {headers : headers}));
     }
     else {
