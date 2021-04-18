83dd62e3507c3d543023b6c6b7953875fcb15dbd
con-ga <58595958+con-ga@users.noreply.github.com>
2021-04-18 14:59:32 +0700

    Add files via upload

---
+++ try/cors-url/cors-url.js
@@ -0,0 +1,21 @@
+self.addEventListener("fetch", function(ev) {
+    const u = ev.request.url.indexOf("?url=");
+    let url;
+    if (u < 0) {
+        const i = ev.request.url.indexOf("?iframe=");
+        let headers = new Headers({"Content-Type" : "text/html"});
+        ifr = ev.request.url.substring(i + 8);
+        let html = "<body><iframe src='" + ifr + "' width=500 height=400></iframe><hr />" + "<iframe width=500 height=400 src='page.x?url=" + ifr + "'></iframe></body>";
+        ev.respondWith(new Response(html, {headers : headers}));
+    }
+    else {
+        url = ev.request.url.substring(u);
+        ev.respondWith(
+            (async function() {
+               const page = await fetch(url, {mode : "no-cors"}); 
+               return page;
+            })()
+        );
+    }
+  
+});
