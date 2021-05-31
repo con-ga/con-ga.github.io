de131c636fe9328cff43dc789421fa72123dcd23
con-ga <58595958+con-ga@users.noreply.github.com>
2021-05-31 20:31:01 +0700

    Update main.js

--- pkmm/main.js
+++ pkmm/main.js
@@ -2,11 +2,8 @@ function tachDanhSach(str)
 {
     let danhSach = str. split(/\s+/).
         filter(s => {
-            /*let bl = s.substr(0, 1) != "#";
-            bl = bl && s.substr(0, 1) != ".";
-            bl = bl && s.substr(0, 1) != "/";
-            */
-            
-            bl = bl && s.match(/\/\./
+            let bl = !s.match(/^[#\/\.]/);
+            bl = bl && !s.match(/\/\./);
+            return bl;
         });
 }
