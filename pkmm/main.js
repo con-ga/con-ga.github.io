3f8265c23032a1aff07f908273628dfef69947aa
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-03 08:09:33 +0700

    Update main.js

--- pkmm/main.js
+++ pkmm/main.js
@@ -7,3 +7,15 @@ function tachDanhSach(str)
             return bl;
         });
 }
+function layNoiDung(noiDung, hamLay)
+{
+    hamLay("danh-sach.htm").then(response =>
+        response.text()
+    ).catch(err => {
+
+    }).then(txt => {
+        return tachDanhSach(txt);
+    }).then(danhSach) => {
+        
+    });
+}
