7589a9cd0cae5b3e56a49aad7ae8d64533432f7c
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-03 15:04:02 +0700

    Update storage.js

--- storage/storage.js
+++ storage/storage.js
@@ -1,1 +1,30 @@
-
+let cache;
+let key;
+function makeStorage(myCache, myKey)
+{
+    cache = myCache;
+    key = myKey;
+}
+function getItem(ten)
+{
+    return cache.match(key).
+        then(response => response.json()
+        ).then(data => data[ten]
+        );
+}
+function setItem(ten, giaTri)
+{
+    let resp;
+    cache.match(key).
+        then(response => {
+            resp = response;
+            return response.json();
+        }).then(data => {
+            if (!data) {
+                data = {};
+            }
+            data[ten] = giaTri;
+            cache.match(key).put(resp.url, new Response(JSON.stringify(data)));
+        })
+}
+export {makeStorage, setItem, getItem};
