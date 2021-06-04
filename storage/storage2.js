e23cb5bab0a2a659ffb86aed83bc0306250c5b35
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-04 08:28:42 +0700

    Add files via upload

---
+++ storage/storage2.js
@@ -0,0 +1,42 @@
+let myCaches;
+const CACHENAME = "datacache";
+const KEY = "dulieu";
+let promiseSet;
+function makeStorage(caches)
+{
+    myCaches = caches;
+}
+function setItem(ten, giaTri)
+{
+    promiseSet = myCaches.open(CACHENAME).
+        then(cache => {
+            return cache.match(KEY);
+        }).
+        then(response => {
+            if (!response) {
+                return {};
+            }
+            return response.json();
+        }).
+        then(data => {
+            data[ten] = giaTri;
+            myCache.open(CACHENAME).
+                then(cache => {
+                   cache.put(KEY, new Response(JSON.stringify(data))); 
+                })
+        }).catch(err);
+}
+function getItem(ten)
+{
+    return promiseSet.then(() => {
+        myCaches.open(CACHENAME).
+            then(cache => cache.match(KEY)).
+            then(response => response.json()).
+            then(data => data[ten]).catch(alert);
+    })
+}
+function echo(msg)
+{
+    alert(msg);
+}
+export {makeStorage, setItem, getItem, echo}
