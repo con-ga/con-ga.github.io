38609bf5db51df3a187a47b6a2e44412c524b2c8
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-17 08:09:34 +0700

    Update app.js

--- mysw/wait-until/app.js
+++ mysw/wait-until/app.js
@@ -3,7 +3,7 @@ self.addEventListener("install", function (evt) {
 });
 
 self.addEventListener("fetch", function (evt) {
-    self.registration.pushNotification("Xin chao (" + evt.request.url + ")",
-    {body : "xyz"});
-    evt.respondWith(new Response("cdefgab<hr />"));
+    /*self.registration.pushNotification("Xin chao (" + evt.request.url + ")",
+    {body : "xyz"});*/
+    evt.respondWith(new Response("cdefgab<hr />" + evt.request.url));
 });
