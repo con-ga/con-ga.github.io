56bb3af7f33640583c00c01b1d68dae6f53ced5f
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-17 09:43:23 +0700

    Update app.js

--- mysw/fetch-respond-with/app.js
+++ mysw/fetch-respond-with/app.js
@@ -3,7 +3,7 @@ self.addEventListener("install", function (evt) {
 });
 
 self.addEventListener("fetch", function (evt) {
-    self.registration.pushNotification("Xin chao (" + evt.request.url + ")",
+    self.registration.showNotification("Xin chao (" + evt.request.url + ")",
     {body : "xyz"});
     //if (!evt.request.url.contains("app2.js")) {
         evt.respondWith(new Response("cdefgab<hr />" + evt.request.url));
