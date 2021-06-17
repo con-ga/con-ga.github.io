add188d641bc31618f2f57f25a73b7e75850c771
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-17 07:38:47 +0700

    Update app.js

--- mysw/wait-until/app.js
+++ mysw/wait-until/app.js
@@ -1,1 +1,7 @@
+self.addEventListener("install", function (evt) {
 
+});
+
+self.addEventListener("fetch", function (evt) {
+    selt.registration.pushNotification("Xin chao (" + evt.request.url + ")");
+});
