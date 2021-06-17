1d68866c9bffd2ab2b5bf6421697bf54acad0425
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-17 09:23:38 +0700

    Create app.js

---
+++ mysw/fetch-respond-with/app.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", function (evt) {
+
+});
+
+self.addEventListener("fetch", function (evt) {
+    /*self.registration.pushNotification("Xin chao (" + evt.request.url + ")",
+    {body : "xyz"});*/
+    //if (!evt.request.url.contains("app2.js")) {
+        evt.respondWith(new Response("cdefgab<hr />" + evt.request.url));
+        return;
+    //}
+    //evt.respondWith(fetch("app2.js"));
+});
+//
