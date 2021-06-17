e69478d3dc04b6b451f78c82fe65bced041cd290
con-ga <58595958+con-ga@users.noreply.github.com>
2021-06-17 09:08:38 +0700

    Update app2.js

--- mysw/wait-until/app2.js
+++ mysw/wait-until/app2.js
@@ -5,10 +5,10 @@ self.addEventListener("install", function (evt) {
 self.addEventListener("fetch", function (evt) {
     /*self.registration.pushNotification("Xin chao (" + evt.request.url + ")",
     {body : "xyz"});*/
-    if (!evt.request.url.contains("app2.js")) {
+    //if (!evt.request.url.contains("app2.js")) {
         evt.respondWith(new Response("cdefgab<hr />" + evt.request.url));
         return;
-    }
-    evt.respondWith(fetch("app2.js"));
+    //}
+    //evt.respondWith(fetch("app2.js"));
 });
 //
