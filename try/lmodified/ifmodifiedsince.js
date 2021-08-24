c3e36a9cb265761f39b3b21e1743647622789512
con-ga <58595958+con-ga@users.noreply.github.com>
2021-08-24 16:48:58 +0700

    Update ifmodifiedsince.js

--- try/lmodified/ifmodifiedsince.js
+++ try/lmodified/ifmodifiedsince.js
@@ -1,7 +1,7 @@
 self.addEventListener("message", function (ev) {
     const headers = new Headers();
     if (ev.data.lmodified != undefined) {
-        headers.append("If-Modified-Since", lmodified);
+        headers.append("If-Modified-Since", ev.data.lmodified);
     }
     fetch(ev.data.url, {headers : headers})
     .then(resp => ev.source.postMessage(resp));
