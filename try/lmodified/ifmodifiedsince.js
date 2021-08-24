e58ae813f56899dacb1bd3a0d729e3dcf8970fb0
con-ga <58595958+con-ga@users.noreply.github.com>
2021-08-24 18:22:10 +0700

    Add files via upload

--- try/lmodified/ifmodifiedsince.js
+++ try/lmodified/ifmodifiedsince.js
@@ -1,8 +1,37 @@
+self.addEventListener("install", (ev) => {
+    ev.waitUntil(
+        caches.open("cache")
+        .then(cache => cache.addAll([
+            "ifmodifiedsince2.htm",
+            "reg.htm",
+            "ifmodifiedsince.js"
+        ]))
+    );
+});
+
+self.addEventListener("fetch", function (ev) {
+    const response = caches.match(ev.request)
+    .then(resp => {
+        if (resp == undefined) {
+            return new Response("{error : \"Không có nội dung\"}");
+        }
+        return resp;
+    });
+    ev.respondWith(response);
+});
+
 self.addEventListener("message", function (ev) {
     const headers = new Headers();
     if (ev.data.lmodified != undefined) {
         headers.append("If-Modified-Since", ev.data.lmodified);
     }
+    let status;
+    let modf;
     fetch(ev.data.url, {headers : headers})
-    .then(resp => ev.source.postMessage(resp));
+    .then(resp => {
+        status = resp.status;
+        modf = resp.headers.get("Date");
+        return resp.text();
+    }).then(txt => ev.source.postMessage({status : status, text : txt, lmodified : modf})
+    );
 });
