528a467b0353793962c4e1f56ff9eeb24de7d8f1
con-ga <58595958+con-ga@users.noreply.github.com>
2026-06-28 16:49:47 +0700

    Refactor Progress class and add alert

--- x-progress/x-progress.js
+++ x-progress/x-progress.js
@@ -73,8 +73,8 @@ class Progress extends HTMLElement {
     const rect = this.getBoundingClientRect();
     // Tránh set width/height = 0 nếu element chưa render ẩn
     //alert(rect.width+","+rect.height);
-    this.canvas.style.width = rect.width || 200;
-    this.canvas.style.height = rect.height || 200;
+    this.canvas.width = rect.width || 200;
+    this.canvas.height = rect.height || 200;
   }
 
   // Getter / Setter cho percent
